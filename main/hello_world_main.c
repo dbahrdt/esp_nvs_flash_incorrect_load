751c96060dfd8e12fd8414c9709d88ea835b7071
Daniel Bahrdt <daniel.bahrdt@eltako.de>
2022-02-07 20:01:17 +0100

    mwe

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -17,7 +17,6 @@
 
 void app_main(void)
 {
-    printf("Hello world!\n");
     nvs_flash_init();
     nvs_handle_t handle;
     nvs_open("test", NVS_READWRITE, &handle);
@@ -25,60 +24,22 @@ void app_main(void)
     for(;true;) {
         const uint8_t u8v = 7;
         const int8_t i8v = -1;
-        uint8_t loadv;
-        char const * str = "test";
+        uint8_t loadv = 10;
         esp_err_t err = ESP_FAIL;
-//         nvs_set_u8(handle, "no_commit", u8v);
+        nvs_set_i8(handle, "notrigger", i8v);
         nvs_commit(handle);
-//         nvs_set_str(handle, "no_commit", str);
-        nvs_set_i8(handle, "no_commit", i8v);
-//         nvs_commit(handle);
-        err = nvs_get_u8(handle, "no_commit", &loadv);
-        printf("Loading u8 without commit: %s value=%d\n", esp_err_to_name(err), (int)loadv);
-        nvs_commit(handle);
-        /*
-        nvs_set_u8(handle, "commit_i8", u8v);
-//         nvs_set_str(handle, "commit_i8", str);
-        nvs_set_i8(handle, "commit_i8", i8v);
-        nvs_commit(handle);
-        err = nvs_get_u8(handle, "commit_i8", &loadv);
-        printf("Loading u8 with commit after set_i8: %s value=%d\n", esp_err_to_name(err), (int)loadv);
+        err = nvs_get_u8(handle, "notrigger", &loadv);
+        printf("err=%s loadv=%d\n", esp_err_to_name(err), (int)loadv);
         nvs_commit(handle);
         
-        nvs_set_u8(handle, "commit_str", u8v);
-//         nvs_set_str(handle, "commit_str", str);
+        nvs_set_u8(handle, "trigger", u8v);
         nvs_commit(handle);
-        nvs_set_i8(handle, "commit_str", i8v);
-        err = nvs_get_u8(handle, "commit_str", &loadv);
-        printf("Loading u8 with commit after set_str: %s value=%d\n", esp_err_to_name(err), (int)loadv);
+        nvs_set_i8(handle, "trigger", i8v);
         nvs_commit(handle);
-        
-        nvs_set_u8(handle, "commit_both", u8v);
-//         nvs_set_str(handle, "commit_both", str);
+        err = nvs_get_u8(handle, "trigger", &loadv);
+        printf("err=%s loadv=%d\n", esp_err_to_name(err), (int)loadv);
         nvs_commit(handle);
-        nvs_set_i8(handle, "commit_both", i8v);
-        nvs_commit(handle);
-        err = nvs_get_u8(handle, "commit_both", &loadv);
-        printf("Loading u8 with commit after set_str and set_i8: %s value=%d\n", esp_err_to_name(err), (int)loadv);
-        
-        nvs_set_u8(handle, "commit_all", u8v);
-        nvs_commit(handle);
-//         nvs_set_str(handle, "commit_all", str);
-        nvs_commit(handle);
-        nvs_set_i8(handle, "commit_all", i8v);
-        nvs_commit(handle);
-        err = nvs_get_u8(handle, "commit_all", &loadv);
-        printf("Loading u8 with commit after set_u8, set_str, set_i8: %s value=%d\n", esp_err_to_name(err), (int)loadv);
-        
-        nvs_set_i8(handle, "wrong load", i8v);
-        nvs_commit(handle);
-        err = nvs_get_u8(handle, "commit_all", &loadv);
-        printf("Loading u8 without prior set_u8: %s value=%d\n", esp_err_to_name(err), (int)loadv);
-        */
-        nvs_commit(handle);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    while(true) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
